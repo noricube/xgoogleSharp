71f2c6ed6bbf5613c6840b5a73e87beb0ba84dd6
Seok-Ju, Yun <noricube@gmail.com>
2014-08-23 16:04:19 +0900

    init commit

---
+++ xgoogleSharp/Browser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace xgoogleSharp
+{
+    class Browser
+    {
+        // https://github.com/pkrumins/xgoogle/blob/master/xgoogle/browser.py 에서 가져온 브라우저 목록
+        protected static string[] UserAgents = {
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.6) Gecko/2009011912 Firefox/3.0.6",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6 (.NET CLR 3.5.30729)",
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.6) Gecko/2009020911 Ubuntu/8.10 (intrepid) Firefox/3.0.6",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6 (.NET CLR 3.5.30729)",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.48 Safari/525.19",
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648)",
+            "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.0.6) Gecko/2009020911 Ubuntu/8.10 (intrepid) Firefox/3.0.6",
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.5) Gecko/2008121621 Ubuntu/8.04 (hardy) Firefox/3.0.5",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-us) AppleWebKit/525.27.1 (KHTML, like Gecko) Version/3.2.1 Safari/525.27.1",
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)",
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)",
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
+        };
+
+        protected Random rand;
+        protected string UserAgent;
+        HttpClient client;
+
+        public Browser()
+        {
+            client = new HttpClient();
+            rand = new Random(System.Threading.Thread.CurrentThread.ManagedThreadId + unchecked((int)System.DateTime.Now.Ticks));
+            SetRandomUserAgent();
+        }
+
+        public void SetRandomUserAgent()
+        {
+            UserAgent = UserAgents[rand.Next(UserAgents.Count())];
+        }
+
+        public async Task<string> FetchPage(string url)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+
+            // 커스텀 헤더 추가
+            requestMessage.Headers.Add("User-Agent", UserAgent);
+            requestMessage.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            requestMessage.Headers.Add("Accept-Language", "en-us,en;q=0.5");
+
+            HttpResponseMessage response = await client.SendAsync(requestMessage);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ xgoogleSharp/GoogleSearch.cs
@@ -0,0 +1,108 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fizzler.Systems.HtmlAgilityPack;
+using System.Web;
+
+namespace xgoogleSharp
+{
+    public class GoogleSearch
+    {
+        protected const string SEARCH_URL_0 = "https://www.google.com/search?hl=en&q={0}&btnG=Google+Search";
+        protected const string NEXT_PAGE_0 = "https://www.google.com/search?hl=en&q={0}&start={1}";
+        protected const string SEARCH_URL_1 = "https://www.google.com/search?hl=en&q={0}&num={1}&btnG=Google+Search";
+        protected const string NEXT_PAGE_1 = "https://www.google.com/search?hl=en&q={0}&num={1}&start={2}";
+
+        public int Page { get; protected set; }
+        public bool HasNext { get; protected set; }
+        protected int RequestPerPage;
+        protected string Query;
+        
+        private Browser Browser;
+
+
+        public GoogleSearch(string query, int requestPerPage = 10)
+        {
+            Query = query;
+            Page = 1;
+            RequestPerPage = requestPerPage;
+            Browser = new Browser();
+        }
+
+        public async Task<List<GoogleSearchResult>> FetchResults()
+        {
+            var results = new List<GoogleSearchResult>();
+
+            string fetchUrl;
+            if ( RequestPerPage == 10)
+            {
+                if (Page == 1)
+                {
+                    fetchUrl = String.Format(SEARCH_URL_0, Query);
+                }
+                else
+                {
+                    fetchUrl = String.Format(NEXT_PAGE_0, Query, (Page-1) * RequestPerPage);
+                }
+            }
+            else
+            {
+                if (Page == 1)
+                {
+                    fetchUrl = String.Format(SEARCH_URL_1, Query, RequestPerPage);
+                }
+                else
+                {
+                    fetchUrl = String.Format(NEXT_PAGE_1, Query, RequestPerPage, (Page - 1) * RequestPerPage);
+                }
+            }
+
+            //fetchUrl = "http://chp.ly.lv/google.html";
+
+            Console.WriteLine(fetchUrl);
+            await Task.Delay(1000);
+                
+            string content = await Browser.FetchPage(fetchUrl);
+
+            var html = new HtmlDocument();
+            html.LoadHtml(content);
+
+            var document = html.DocumentNode;
+
+            // 모든 글 선택
+            foreach (var node in document.QuerySelectorAll("li.g"))
+            {
+                var titleNode = node.QuerySelector("h3.r a");
+                var descNode = node.QuerySelector("span.st");
+
+                if (descNode == null )
+                {
+                    continue;
+                }
+                Console.WriteLine(node.InnerText);
+                results.Add(new GoogleSearchResult()
+                {
+                    Title = HttpUtility.HtmlDecode(titleNode.InnerText),
+                    Description = HttpUtility.HtmlDecode(descNode.InnerText),
+                    Url = titleNode.Attributes["href"].Value
+                });
+            }
+
+            // 다음글 있는지 확인
+            HasNext = false;
+            foreach (var node in document.QuerySelectorAll("span"))
+            {
+                if (node.InnerText.Equals("Next"))
+                {
+                    HasNext = true;
+                    Page++;
+                }
+            }
+
+            return results;
+        }
+    }
+}
